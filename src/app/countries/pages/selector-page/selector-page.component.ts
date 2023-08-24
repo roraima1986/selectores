ae0d15ab870fc8dab4f2db92f49fb8deb703e33c
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-24 11:08:59 -0400

    Formulario reactivo - primer selector

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class SelectorPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  })
+
+  constructor(
+    private fb: FormBuilder,
+  ){}
 }
