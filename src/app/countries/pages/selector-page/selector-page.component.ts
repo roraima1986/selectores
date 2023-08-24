74369a68e5dc0f89a72f61d8162e9c3449951a7e
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-24 11:57:20 -0400

    Selector de regiones

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Region } from '../../interfaces/country.interface';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -17,5 +19,10 @@ export class SelectorPageComponent {
 
   constructor(
     private fb: FormBuilder,
+    private countriesService: CountriesService
   ){}
+
+  get regions(): Region[]{
+    return this.countriesService.regions;
+  }
 }
