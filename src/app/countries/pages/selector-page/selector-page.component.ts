1db60f272aeef9f2079d203c9b157cfb554ed2ed
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-28 10:49:01 -0400

    Tercer selector anidado - Fronteras

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { switchMap, tap } from 'rxjs';
+import { filter, Observable, switchMap, tap } from 'rxjs';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
@@ -13,11 +13,12 @@ import { CountriesService } from '../../services/countries.service';
 export class SelectorPageComponent implements OnInit {
 
   public countriesByRegion: SmallCountry[] = [];
+  public borders: string[] = [];
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
-    borders: ['', Validators.required],
+    border: ['', Validators.required],
   })
 
   constructor(
@@ -27,6 +28,7 @@ export class SelectorPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRegionChanged();
+    this.onCountryChanged();
   }
 
   get regions(): Region[]{
@@ -43,4 +45,18 @@ export class SelectorPageComponent implements OnInit {
         this.countriesByRegion = countries;
       })
   }
+
+  onCountryChanged():void{
+    this.myForm.get('country')!.valueChanges
+      .pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value:string) => value.length > 0),
+        switchMap( (alphaCode) => this.countriesService.getCountryByAlphaCode(alphaCode))
+      )
+      .subscribe(country => {
+        this.borders = country.borders;
+      })
+  }
+
+
 }
